e22a52fbd1084ef0be4eaec4a7efed0e32ad2695
Gordon Gibb <g.gibb@epcc.ed.ac.uk>
2018-11-09 16:52:19 +0000

    Added functionality in writeparcels to write with a fixed time interval rather than a fixed number of timesteps

--- components/writeparcels/src/writeparcels.F90
+++ components/writeparcels/src/writeparcels.F90
@@ -4,7 +4,7 @@ module writeparcels_mod
   use datadefn_mod, only : DEFAULT_PRECISION, PARCEL_INTEGER, LONG_INTEGER
   use state_mod, only: model_state_type
   use monc_component_mod, only: component_descriptor_type
-  use optionsdatabase_mod, only : options_get_integer,options_get_logical
+  use optionsdatabase_mod, only : options_get_integer,options_get_logical,options_get_string,options_get_real
   use parcel_interpolation_mod, only : x_coords, y_coords, z_coords, par2grid, cache_parcel_interp_weights
   use timer_mod, only: register_routine_for_timing, timer_start, timer_stop
 
@@ -12,7 +12,11 @@ module writeparcels_mod
 
   integer :: num, ppersteps, gpersteps, pwritten, gwritten
 
-  integer :: handlep, handleg
+  integer :: handlep, handleg, ierr
+
+  real(kind=DEFAULT_PRECISION) :: dtparcels, dtgrids, tnextgrids, tnextparcels
+
+  CHARACTER(len=4) :: mode
 
 contains
 
@@ -28,10 +32,37 @@ contains
   subroutine initialisation_callback(state)
     type(model_state_type), intent(inout), target :: state
 
+
     if (state%parallel%my_rank .eq. 0) print *, "Writer initialisation"
 
-    ppersteps=options_get_integer(state%options_database,"parcel_dump_frequency")
-    gpersteps=options_get_integer(state%options_database,"grid_dump_frequency")
+    mode = options_get_string(state%options_database,"writeparcels_mode")
+
+    if (state%parallel%my_rank .eq. 0) then
+      print *, "Selected writing mode: ",mode
+    endif
+
+    if (mode .eq. "time") then
+      dtparcels = options_get_real(state%options_database,"parcel_time_write_frequency")
+      dtgrids = options_get_real(state%options_database,"grid_time_write_frequency")
+      tnextgrids=0.
+      tnextparcels=0.
+    else if (mode .eq. "step") then
+      ppersteps=options_get_integer(state%options_database,"parcel_step_write_frequency")
+      gpersteps=options_get_integer(state%options_database,"grid_step_write_frequency")
+    else if (mode .eq. "none") then
+      if (state%parallel%my_rank .eq. 0) print *, "No grid/parcel files will be produced"
+    else
+      if (state%parallel%my_rank .eq. 0) then
+        print *, "Error: mode '",mode,"' not recognised."
+        print *, "The valid options are 'none', 'time' or 'step'"
+        print *, 'Aborting'
+      endif
+      call mpi_finalize(ierr)
+      stop
+    endif
+
+
+
 
     num=state%iterations
     pwritten=0
@@ -54,45 +85,44 @@ contains
 
     num=state%iterations
 
+    if (mode .eq. "step") then
 
-    if (ppersteps .ne. 0) then
-      if(mod(num,ppersteps) .eq. 0) then
-        call timer_start(handlep)
-        call write_parcels_to_file(state)
-        pwritten=pwritten+1
-        call timer_stop(handlep)
+      if (ppersteps .ne. 0) then
+        if(mod(num,ppersteps) .eq. 0) then
+          call timer_start(handlep)
+          call write_parcels_to_file(state)
+          pwritten=pwritten+1
+          call timer_stop(handlep)
+        endif
       endif
-    endif
 
-    if (gpersteps .ne. 0) then
-      if (mod(num,gpersteps) .eq. 0) then
+      if (gpersteps .ne. 0) then
+        if (mod(num,gpersteps) .eq. 0) then
+          call timer_start(handleg)
+          call write_grids_to_file(state)
+          gwritten=gwritten+1
+          call timer_stop(handleg)
+        endif
+      endif
 
+    else if (mode .eq. "time") then
 
-        call cache_parcel_interp_weights(state)
-        call par2grid(state,state%parcels%b,state%b)
-        call par2grid(state,state%parcels%p,state%p)
-        call par2grid(state,state%parcels%q,state%q)
-        call par2grid(state,state%parcels%r,state%r)
-
-        ! obtain the humidity and liquid humidity
-        call par2grid(state,state%parcels%h,state%hg)
-        !$OMP PARALLEL DO
-        do i=1,size(state%hgliq%data,3)
-          do j=1,size(state%hgliq%data,2)
-            do k=1,size(state%hgliq%data,1)
-              state%hgliq%data(k,j,i) = max(0.,state%hg%data(k,j,i) - exp(-z_coords(k)))
-              state%b%data(k,j,i) = state%b%data(k,j,i) + 12.5*state%hgliq%data(k,j,i)
-            enddo
-          enddo
-        enddo
-        !$OMP END PARALLEL DO
-
-
+      if (state%time .ge. tnextgrids) then
         call timer_start(handleg)
         call write_grids_to_file(state)
         gwritten=gwritten+1
         call timer_stop(handleg)
+        tnextgrids = tnextgrids + dtgrids
       endif
+
+      if (state%time .ge. tnextparcels) then
+        call timer_start(handlep)
+        call write_parcels_to_file(state)
+        pwritten=pwritten+1
+        call timer_stop(handlep)
+        tnextparcels = tnextparcels + dtparcels
+      endif
+
     endif
 
     num=num+1
@@ -174,12 +204,32 @@ contains
   end subroutine
 
   subroutine write_grids_to_file(state)
-    type(model_state_type), intent(in) :: state
+    type(model_state_type), intent(inout) :: state
     character (len=21) :: filename
     character (len=22) :: fnamedummy
     integer :: proc
     integer :: i1, i2, j1, j2, k1, k2
+    integer :: i, j, k
     real(kind=DEFAULT_PRECISION) :: x1, x2, y1, y2, z1, z2
+
+    call cache_parcel_interp_weights(state)
+    call par2grid(state,state%parcels%b,state%b)
+    call par2grid(state,state%parcels%p,state%p)
+    call par2grid(state,state%parcels%q,state%q)
+    call par2grid(state,state%parcels%r,state%r)
+
+    ! obtain the humidity and liquid humidity
+    call par2grid(state,state%parcels%h,state%hg)
+    !$OMP PARALLEL DO
+    do i=1,size(state%hgliq%data,3)
+      do j=1,size(state%hgliq%data,2)
+        do k=1,size(state%hgliq%data,1)
+          state%hgliq%data(k,j,i) = max(0.,state%hg%data(k,j,i) - exp(-z_coords(k)))
+          state%b%data(k,j,i) = state%b%data(k,j,i) + 12.5*state%hgliq%data(k,j,i)
+        enddo
+      enddo
+    enddo
+    !$OMP END PARALLEL DO
 
     i1=state%local_grid%local_domain_start_index(1)
     i2=state%local_grid%local_domain_end_index(1)
